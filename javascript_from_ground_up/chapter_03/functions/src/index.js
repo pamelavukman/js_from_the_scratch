c584d7f5580d3ff3ed2ed5fb0f8cf22497e7a92d
ahmedbhameed <contact@ahmedhameed.dev>
2022-07-23 21:33:39 +0200

    Expand few functions

--- javascript_from_ground_up/chapter_03/functions/src/index.js
+++ javascript_from_ground_up/chapter_03/functions/src/index.js
@@ -61,7 +61,7 @@ function shuffleMyArray(arr) {
  */
 
 function reverseNum(num) {
-  return +num.toString().split('').reverse().join('');
+  // your code here
 }
 
 /**
@@ -72,24 +72,7 @@ function reverseNum(num) {
  */
 
 function countVowel(str) {
-  // One line solution
-  // return (str.match(/[aeiou]/gi) || []).length
-
-  // Multi line solution
-  var count = 0;
-  str = str.toLowerCase();
-  for (var i = 0; i < str.length; i++) {
-    if (
-      str.charAt(i) == 'a' ||
-      str.charAt(i) == 'e' ||
-      str.charAt(i) == 'i' ||
-      str.charAt(i) == 'o' ||
-      str.charAt(i) == 'u'
-    ) {
-      count++; //Increment vowel count
-    }
-  }
-  return count;
+  // your code here
 }
 
 /**
@@ -106,13 +89,7 @@ function countVowel(str) {
  */
 
 function checkPalindrome(str) {
-  for (var i = 0; i < str.length; i++) {
-    if (str.charAt(i) != str.charAt(str.length - i - 1)) {
-      // Comparison fail
-      return false;
-    }
-  }
-  return true;
+  // Your code here
 }
 
 /**
@@ -123,10 +100,7 @@ function checkPalindrome(str) {
  */
 
 function getFactorial(num) {
-  if (num === 1) return 1;
-  if (num === 0 || num < 0) return 0;
-
-  return num * getFactorial(num - 1);
+  // Your code here
 }
 
 // Do not touch this line
